7efff68837d5d84e6143dfb4c47d36578ebc39b7
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-03-01 13:21:57 -0600

    Fix typos and reword in How It Works

--- ror/client/src/components/HowItWorks.js
+++ ror/client/src/components/HowItWorks.js
@@ -14,9 +14,9 @@ function HowItWorks() {
       <h4>Harmonic</h4>
       <ul>
         <li>Vocing: Voices may not pass each other. For example the alto cannot rise higher than the soprano on any given beat.</li>
-        <li>Harmony: Each note must fall within the harmony of the piece. Harmonies are determined by both the harmonic context created by the other voices on any given beat, as well as <a target="_blank" href="https://www.musicnotes.com/now/wp-content/uploads/1-3.png">acceptable chord progressions</a> from beat to beat.</li>
+        <li>Harmony: Each note must fall within the harmony of the piece. Harmonies are determined by the harmonic context created by the other voices on any given beat, as well as <a target="_blank" href="https://www.musicnotes.com/now/wp-content/uploads/1-3.png">acceptable chord progressions</a> from beat to beat.</li>
         <li>Parallel harmony: Perfect 5ths and octaves may not move in parallel motion. For example, if alto is on middle C and soprano is an octave higher, they may not both move in the same interval (e.g. down a step), instead they must move in different intervals. Soprano moving down 3 steps and alto moving down 1 step would be acceptable, for example.</li>
-        <li>Similar harmony: Similar motion into perfect intervals (5ths and octaves) is not allowed. If two voicing are moving into a perfect interval from each other, they must move in contrary motion - one moves up and one moves down.</li>
+        <li>Similar harmony: Similar motion into perfect intervals (5ths and octaves) is not allowed. If two voices are moving into a perfect interval from each other, they must move in contrary motion - one moves up and one moves down.</li>
         <li>Dissonant harmony: Dissonant harmonies (2nds, 4ths, and 7ths) between soprano and bass are disallowed. In 2-part harmony, all dissonant harmonies are disallowed, but in 4-part harmony, this is ok between inner voices.</li>
         <li>Bass notes: The first and last notes in the bass line must be the root of the chord.</li>
         <li>Soprano leading tone: If the soprano's second to last note is B, the last note must be C. This is because B is the leading tone and the leading tone should always go up. In 4-part harmony, it is still recommended that leading tones go up, but it is only required in the soprano, especially at the end of the piece, because the soprano is more exposed.</li>
@@ -24,7 +24,7 @@ function HowItWorks() {
       <h4>Melodic</h4>
       <ul>
         <li>Range: Each voice cannot travel outside it's acceptable range.</li>
-        <li>Tritone runs: A voice may not melodically outline a triton (an interval exactly 6 half-steps away). In otherwords, if a voice has F, G, A, B, the following note cannot be A, because that would outline a tritone.</li>
+        <li>Tritone runs: A voice may not melodically outline a tritone (an interval exactly 6 half-steps away). In otherwords, if a voice has F, G, A, B, the following note cannot be A, because moving from F to B and back down would outline a tritone.</li>
         <li>Tritone skips: A voice may not leap in an interval of a tritone. For example, a voice cannot skip from a B to an F or vice versa.</li>
         <li>Large skips: A voice may not skip in intervals greater than an octave.</li>
         <li>After skips: After a voice skips in any direction, it must then either step in the opposite direction or remain on the same note (unison).</li>
